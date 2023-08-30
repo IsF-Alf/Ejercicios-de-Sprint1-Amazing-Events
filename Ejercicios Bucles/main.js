748d3e066adfc5521e962766ed2b6000ee834a00
Isaac Alfonso <ifa24991@gmail.com>
2023-08-30 16:11:30 -0300

    Comienzo ejercicios de bucles

---
+++ Ejercicios Bucles/main.js
@@ -0,0 +1,69 @@
+// Ejercicios de Bucles
+
+
+// --------------  Ejercicio N°1  --------------;
+console.log("- Ejercicio 1");
+let numeroIngresado = parseInt(prompt(`Ingresa un numero entre 1 y 100:`));
+if (numeroIngresado >= 1 && numeroIngresado <= 100) {
+    console.log(`Números entre ${numeroIngresado} y 0:`);
+    for (let i = numeroIngresado; i >= 0; i--) {
+        console.log(i);
+    };
+} else {
+    console.log(`El numero ${numeroIngresado} no esta entre 1 y 100`);
+};
+
+// --------------  Ejercicio N°2  --------------;
+console.log("- Ejercicio 2");
+
+
+// --------------  Ejercicio N°3  --------------;
+console.log("- Ejercicio 3");
+
+
+// --------------  Ejercicio N°4  --------------;
+console.log("- Ejercicio 4");
+
+
+// --------------  Ejercicio N°5  --------------;
+console.log("- Ejercicio 5");
+
+
+// --------------  Ejercicio N°6  --------------;
+console.log("- Ejercicio 6");
+
+
+// --------------  Ejercicio N°7  --------------;
+console.log("- Ejercicio 7");
+
+
+// --------------  Ejercicio N°8  --------------;
+console.log("- Ejercicio 8");
+
+
+// --------------  Ejercicio N°2  --------------;
+console.log("- Ejercicio 2");
+
+
+// --------------  Ejercicio N°3  --------------;
+console.log("- Ejercicio 3");
+
+
+// --------------  Ejercicio N°4  --------------;
+console.log("- Ejercicio 4");
+
+
+// --------------  Ejercicio N°5  --------------;
+console.log("- Ejercicio 5");
+
+
+// --------------  Ejercicio N°6  --------------;
+console.log("- Ejercicio 6");
+
+
+// --------------  Ejercicio N°7  --------------;
+console.log("- Ejercicio 7");
+
+
+// --------------  Ejercicio N°8  --------------;
+console.log("- Ejercicio 8");
